2b9db7336a8a1277e30327db2b5ffb576023c28e
KyleMalinowski <kmalino66@gmail.com>
2016-03-26 22:20:50 -0500

    Initial Commit
    n/a

---
+++ WorkNotesTracker/Form1.Designer.vb
@@ -0,0 +1,139 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.button_clear = New System.Windows.Forms.Button()
+        Me.button_copy = New System.Windows.Forms.Button()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.TemplatesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CopyToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.Info
+        Me.TextBox1.Location = New System.Drawing.Point(12, 27)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox1.Size = New System.Drawing.Size(868, 430)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = "~"
+        '
+        'button_clear
+        '
+        Me.button_clear.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.button_clear.Location = New System.Drawing.Point(805, 463)
+        Me.button_clear.Name = "button_clear"
+        Me.button_clear.Size = New System.Drawing.Size(75, 23)
+        Me.button_clear.TabIndex = 1
+        Me.button_clear.Text = "Clear"
+        Me.button_clear.UseVisualStyleBackColor = True
+        '
+        'button_copy
+        '
+        Me.button_copy.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.button_copy.Location = New System.Drawing.Point(724, 463)
+        Me.button_copy.Name = "button_copy"
+        Me.button_copy.Size = New System.Drawing.Size(75, 23)
+        Me.button_copy.TabIndex = 2
+        Me.button_copy.Text = "Copy"
+        Me.button_copy.UseVisualStyleBackColor = True
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.TemplatesToolStripMenuItem, Me.CopyToolStripMenuItem, Me.ClearToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(892, 24)
+        Me.MenuStrip1.TabIndex = 3
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'TemplatesToolStripMenuItem
+        '
+        Me.TemplatesToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SaveToolStripMenuItem, Me.OpenToolStripMenuItem})
+        Me.TemplatesToolStripMenuItem.Name = "TemplatesToolStripMenuItem"
+        Me.TemplatesToolStripMenuItem.Size = New System.Drawing.Size(73, 20)
+        Me.TemplatesToolStripMenuItem.Text = "Templates"
+        '
+        'SaveToolStripMenuItem
+        '
+        Me.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
+        Me.SaveToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.SaveToolStripMenuItem.Text = "Save"
+        '
+        'OpenToolStripMenuItem
+        '
+        Me.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
+        Me.OpenToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.OpenToolStripMenuItem.Text = "Open"
+        '
+        'CopyToolStripMenuItem
+        '
+        Me.CopyToolStripMenuItem.Name = "CopyToolStripMenuItem"
+        Me.CopyToolStripMenuItem.Size = New System.Drawing.Size(47, 20)
+        Me.CopyToolStripMenuItem.Text = "Copy"
+        '
+        'ClearToolStripMenuItem
+        '
+        Me.ClearToolStripMenuItem.Name = "ClearToolStripMenuItem"
+        Me.ClearToolStripMenuItem.Size = New System.Drawing.Size(46, 20)
+        Me.ClearToolStripMenuItem.Text = "Clear"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(892, 498)
+        Me.Controls.Add(Me.button_copy)
+        Me.Controls.Add(Me.button_clear)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Work Notes"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents button_clear As Button
+    Friend WithEvents button_copy As Button
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents TemplatesToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CopyToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ClearToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ WorkNotesTracker/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+    Private Sub button_clear_Click(sender As Object, e As EventArgs) Handles button_clear.Click
+        TextBox1.Text = "~"
+        TextBox1.SelectionStart = TextBox1.Text.Length + 1
+        TextBox1.Focus()
+    End Sub
+
+    Private Sub button_copy_Click(sender As Object, e As EventArgs) Handles button_copy.Click
+        My.Computer.Clipboard.SetText(TextBox1.Text)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        TextBox1.Text = "~"
+        TextBox1.SelectionStart = TextBox1.Text.Length + 1
+    End Sub
+
+    Private Sub TextBox1_KeyUp(sender As Object, e As KeyEventArgs) Handles TextBox1.KeyUp
+        If e.KeyCode = 13 Then
+            My.Computer.Keyboard.SendKeys("{~}")
+        End If
+    End Sub
+End Class
