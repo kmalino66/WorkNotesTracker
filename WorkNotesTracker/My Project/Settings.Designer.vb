b6aee6ea6590e768f2520d356167cb648dfa911f
KyleMalinowski <kmalino66@gmail.com>
2016-05-19 21:33:02 -0500

    Clean up

--- WorkNotesTracker/My Project/Settings.Designer.vb
+++ WorkNotesTracker/My Project/Settings.Designer.vb
@@ -68,21 +68,10 @@ Namespace My
         <Global.System.Configuration.ApplicationScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
          Global.System.Configuration.SpecialSettingAttribute(Global.System.Configuration.SpecialSetting.ConnectionString),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("Data Source=F:\Dropbox\Dropbox\Programming\Visual Basic\WorkNotesTracker\Filament"& _ 
-            "Information.sdf;")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("Data Source=|DataDirectory|\FilamentInformation.sdf;")>  _
         Public ReadOnly Property TemplateConnection() As String
             Get
                 Return CType(Me("TemplateConnection"),String)
-            End Get
-        End Property
-        
-        <Global.System.Configuration.ApplicationScopedSettingAttribute(),  _
-         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.SpecialSettingAttribute(Global.System.Configuration.SpecialSetting.ConnectionString),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("Data Source=|DataDirectory|\FilamentInformation.db;")>  _
-        Public ReadOnly Property Setting() As String
-            Get
-                Return CType(Me("Setting"),String)
             End Get
         End Property
     End Class
